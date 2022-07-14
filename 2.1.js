453ec9cca56a6f0cf9535c9d0e3e641b8c2447a5
Anthony <anthony.denis.becode@gmail.com>
2022-07-14 15:29:05 +0200

    FINSIH

---
+++ 2.1.js
@@ -0,0 +1,10 @@
+    let result = prompt("What's your âge ?")
+    
+    if(result < 18)
+    {
+      console.log("You are not an adult");
+    }
+    else
+    {
+       console.log("You are an adult");
+    }
